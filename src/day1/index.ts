8299bc34063441c3a0ba948b8405b22c17096ab2
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-12-01 12:46:17 +0300

    Solve day 1 part 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,1 +1,33 @@
-// start here
+import { readFile } from "fs/promises";
+import { resolve } from "path";
+
+const INPUT_FILENAME = resolve(__dirname, "input");
+
+const main = async () => {
+  const input = await readFile(INPUT_FILENAME, { encoding: "utf-8" });
+  const allCalories = input.split("\n");
+
+  let highestCalories = 0;
+  let tempCaloriesGroup: number[] = [];
+
+  allCalories.forEach((calories, index) => {
+    if (calories !== "") {
+      tempCaloriesGroup.push(Number(calories));
+      return;
+    }
+
+    const sumOfCalories = tempCaloriesGroup.reduce((acc, curr) => acc + curr);
+
+    if (sumOfCalories > highestCalories) {
+      highestCalories = sumOfCalories;
+    }
+
+    tempCaloriesGroup = [];
+  });
+
+  console.log(highestCalories);
+};
+
+main().catch((error) => {
+  process.exit(1);
+});
